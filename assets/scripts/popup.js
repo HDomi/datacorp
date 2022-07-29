450c1da040412eeef425cc4f6eee4895b9330330
HDomi <84692508+HDomi@users.noreply.github.com>
2022-07-29 09:51:29 +0900

    zz

---
+++ assets/scripts/popup.js
@@ -0,0 +1,14 @@
+$(function() {
+    $(".find_pass").click(function() {
+        $(".popup_wrap").fadeIn();
+    });
+
+    $(".ic_close").click(function() {
+        $(".popup_wrap").fadeOut();
+    });
+    $(".cl_pop").click(function() {
+        $(".popup_wrap").fadeOut();
+    });
+    
+
+});
